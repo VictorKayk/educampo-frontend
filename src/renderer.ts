73a07601ede79729a25393637314bd0bacaa3dd3
Victor Kayk <victorkayk77@gmail.com>
2023-03-12 17:05:29 -0300

    chore: add additional configs

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,9 +26,5 @@
  * ```
  */
 
-
-
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
-
 // Add this to the end of the existing file
-import './app';
+import './main';
