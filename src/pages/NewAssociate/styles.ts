c28cad393e42c80335d1e832f9bb3763a8273f4b
Victor Kayk <victorkayk77@gmail.com>
2023-03-14 11:11:30 -0300

    chore: add devContentSecurityPolicy in forge.config

---
+++ src/pages/NewAssociate/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  padding: 0 32px;
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+export const SearchBarButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 80px;
+`;
